a99fb8b956d2cda38ee360e108f7b790a2c9b3a3
Мухаммадчон Пулатов Хамрокулович <mpulatov@fardo.lc>
2022-07-21 17:38:13 +0500

    added Kanban page and License Key for @syncfusion

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./context/ContextProvider";
 import App from "./App";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+// Registering Syncfusion license key
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhiQlFadVlJVXxLfkx0RWFbb1l6d1FMY1pBNQtUQF1hS35UdERjXnpcdHJRTmRd"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
