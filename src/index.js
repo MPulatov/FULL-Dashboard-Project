a75767c763b36b715dc333d47d461c04d74946b7
Мухаммадчон Пулатов Хамрокулович <mpulatov@fardo.lc>
2022-07-21 13:14:41 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "./context/ContextProvider";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
